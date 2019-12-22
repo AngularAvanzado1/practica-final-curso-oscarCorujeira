f354f5d11de903e799eb330f362d37d94dbc7a26
oscar.corujeira <oscar.corujeira@gmail.com>
2019-12-22 16:18:59 +0100

    Cambios tests ok

--- pf/apps/banco/src/app/banco/home-filter-ui/home-filter-ui.component.spec.ts
+++ pf/apps/banco/src/app/banco/home-filter-ui/home-filter-ui.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 import { HomeFilterUiComponent } from './home-filter-ui.component';
 
 describe('HomeFilterUiComponent', () => {
   let component: HomeFilterUiComponent;
   let fixture: ComponentFixture<HomeFilterUiComponent>;
+  // create new instance of FormBuilder
+  const formBuilder: FormBuilder = new FormBuilder();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeFilterUiComponent ]
+      imports: [ReactiveFormsModule],
+      declarations: [ HomeFilterUiComponent ],
+      providers: [
+        { provide: FormBuilder, useValue: formBuilder }
+      ],
     })
     .compileComponents();
   }));
--- pf/apps/banco/src/app/banco/home/home.component.spec.ts
+++ pf/apps/banco/src/app/banco/home/home.component.spec.ts
@@ -1,23 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HomeComponent } from './home.component';
 import { HomeUiComponent } from '../home-ui/home-ui.component';
+import { HomeFilterUiComponent } from '../home-filter-ui/home-filter-ui.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
+  let componentFilter: HomeFilterUiComponent;
+  let componentUi: HomeUiComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let fixtureFilter: ComponentFixture<HomeFilterUiComponent>;
+  let fixtureUi: ComponentFixture<HomeUiComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],
-      declarations: [ HomeComponent, HomeUiComponent ]
+      imports: [HttpClientTestingModule, RouterTestingModule, ReactiveFormsModule ],
+      declarations: [ HomeComponent, HomeUiComponent, HomeFilterUiComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    fixtureFilter = TestBed.createComponent(HomeFilterUiComponent);
+    componentFilter = fixtureFilter.componentInstance;
+    fixtureFilter.detectChanges();
+
+    fixtureUi = TestBed.createComponent(HomeUiComponent);
+    componentUi = fixtureUi.componentInstance;
+    fixtureUi.detectChanges();
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
