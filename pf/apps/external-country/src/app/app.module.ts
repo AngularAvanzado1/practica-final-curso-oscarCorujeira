4337f3b865665ddf7d44b58b52edeaff95d75712
oscar.corujeira <oscar.corujeira@gmail.com>
2019-12-22 19:21:01 +0100

    Crear vanilla con web component

--- pf/apps/external-country/src/app/app.module.ts
+++ pf/apps/external-country/src/app/app.module.ts
@@ -1,7 +1,21 @@
+import { PaisComponent, WebcompModule } from '@pf/shared/webcomp';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import 'zone.js';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, WebcompModule],
+  entryComponents: [PaisComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const el = createCustomElement(PaisComponent, {
+      injector: this.injector
+    });
+    customElements.define('wbde-country', el);
+  }
+}
